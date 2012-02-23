0e459a772db02b0e8ce5b81e11cbc44139ece2d1
Michael Watts <mike@altescape.co.uk>
2012-02-23 19:46:49 +0000

    Updates

--- apple_mouse_delta_tracking/mouse-wheel-event.js
+++ apple_mouse_delta_tracking/mouse-wheel-event.js
@@ -7,51 +7,32 @@ Array.min = function( array ){
 
 window.onload = function(){
 	
-	/*
-	var scroller = document.getElementById('threesixty');
-	var scrolloutput = document.getElementById('scrolloutput');
-	setTimeout(function(){
-		scroller.addEventListener('onscroll',liMouseOver(1),false);
-	},50);
-	
-	function liMouseOver(e){
-		//$('#scrollOutput').innerHTML();
-		e = e + 1;
-		console.log(e);
-	}
-	*/
-	/** This is high-level function.
-	 * It must react to delta being more/less than zero.
-	 */
+	var up = document.getElementById('up');
+	var down = document.getElementById('down');
+	var right = document.getElementById('right');
+	var left = document.getElementById('left');
 	
 	var hoz = 0, vert = 0;
-	function createshit(hoz,vert){
-		$('#styleHolder').html('<style>'+
-			'body{box-shadow:0 0 '+hoz+'px rgba(255,255,255,'+vert+') inset}'+
-			'#output{'+
-				'left:'+hoz+'px;'+
-				'top:'+vert+'px}'+
-			'#debug{'+
-				'left:'+hoz+'px;'+
-				'top:'+vert+'px;'+
-				 'background:rgba('+vert+','+vert+','+vert+', 1)}'+
-			'#debug p{'+
-				'-webkit-transform:rotate('+hoz+'deg);'+
-				'top:'+vert+'px;'+
-				'box-shadow: 0 0 0 '+hoz*0.1+'px rgba('+hoz+','+hoz+','+hoz+', 0.1);'+
-				'letter-spacing: '+hoz*0.2+'px}'+
-			'</style>');
-	}
 	
 	function handle(delta) {
 			hoz = Math.floor(hoz + event.wheelDeltaX*0.073);
 			vert = Math.floor(vert + event.wheelDeltaY*0.02);
 			
+			hozSimp = event.wheelDeltaX;
+			vertSimp = event.wheelDeltaY;
+			
 			if (delta){
-				console.log(delta);
-				createshit(hoz,vert);
+				styleContent(hoz,vert);
+				mouseDir(hozSimp, vertSimp);
 			}
 	        
+	}
+	function styleContent(hoz,vert){
+		$('#styleHolder').html('<style>'+
+			'#output{'+
+				'left:'+hoz+'px;'+
+				'top:'+vert+'px}'+
+			'</style>');
 	}
 
 	/** Event handler for mouse wheel event.
@@ -71,11 +52,7 @@ window.onload = function(){
 	         * and negative, if wheel was scrolled down.
 	         */
 	        if (delta)
-	                //setTimeout(handle(delta),100);
 					handle(delta);
-					//number();
-					//console.log( number() );
-					//addElements();
 	        /** Prevent default actions caused by mouse wheel.
 	         * That might be ugly, but we handle scrolls somehow
 	         * anyway, so don't bother here..
@@ -83,7 +60,7 @@ window.onload = function(){
 	        if (event.preventDefault)
 	                event.preventDefault();
 		event.returnValue = false;
-	}
+	}		
 
 	/** Initialization code. 
 	 * If you use your own event management code, change it as required.
@@ -97,22 +74,60 @@ window.onload = function(){
 
 function number(){
 	if(event.wheelDelta)
-	return Math.abs(event.wheelDelta);
+		return Math.abs(event.wheelDelta);
 	else
-	return 0;
+		return 0;
 }
-var arr = new Array;
 
-function addElements(){
+function mouseDir(dx,dy){
 	
-	console.log('method testing: '+ event.type);
-	// get data 
+	var threshold = 5;
+	var north = dy < -threshold;
+	var south = dy > threshold;
+	var east = dx > threshold;
+	var west = dx < -threshold;
 	
-	arr.push(number());
-	//console.log(Math.floor(number()/8));
-	var output = document.getElementById('output');
-	var txt = '<p id="last" style="height:'+ number()/4 +'px">'+ number() +'</p>';
-	output.innerHTML = txt;
-	//	output.appendChild('<p>');
+	// EAST 
+	if(east && !(north) && !(south)){
 		
+		document.body.className += ' hl_e';		
+	}
+	// WEST 
+	if(west && !(north) && !(south)){
+		document.body.className += ' hl_w';
+	}
+	// SOUTH 
+	if(south && !(east) && !(west)){
+		document.body.className += ' hl_n';
+	}
+	// NORTH 
+	if(north && !(east) && !(west)){
+		document.body.className += ' hl_s';
+	}
+	// NORTH EAST
+	if (north && east){
+		document.body.className += ' hl_ne';
+	}
+	// NORTH WEST
+	if (north && west){
+		document.body.className += ' hl_nw';
+	}
+	// SOUTH EAST
+	if (south && east){
+		document.body.className += ' hl_se';
+	}
+	// SOUTH WEST
+	if (south && west){
+		document.body.className += ' hl_sw';
+	}
+	
+	
+	var myTimer = setTimeout(function(){
+		document.body.className = '';
+	}, 1000);
+	
+	// NOTES:
+	// Wouldnt it be cool to actually record the mouse movement?
+	//for this direction would have to be recorded and the velocity
+	
 }
